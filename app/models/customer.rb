116f46d9073d3fcf4bedfb696fae6cc060402cd8
dchase87 <david.chase4@gmail.com>
2017-05-08 10:36:18 -0400

    Done.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,37 @@
 class Customer
-  attr_accessor :first_name, :last_name
+  attr_accessor :first_name, :last_name, :reviews
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
+    @reviews = []
   end
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.detect {|customer| customer.full_name == name}
+  end
+
+  def self.find_all_by_first_name(name)
+    self.all.map {|customer| customer.first_name == name}
+  end
+
+  def self.all_names
+    self.all.map {|customer| customer.full_name}
+  end
+
+  def add_review(restaurant, content)
+    review = Review.new(self, restaurant, content)
+    self.reviews << review
+    restaurant.reviews << review
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,28 @@
 class Restaurant
   attr_accessor :name
+  attr_reader :reviews
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+    @reviews = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.detect {|restaurant| restaurant.name == name}
+  end
+
+  def reviews
+    self.reviews
+  end
+
+  def customers
+    self.reviews.map {|review| review.customer}
   end
 
 end
